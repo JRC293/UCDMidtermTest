6b225b2f670388a29a8eb049b17e2a637d020690
joshuacork293@yahoo.com <>
2018-11-12 14:58:02 -0500

    This is a commit for the test

---
+++ src/ucdmidtermtest/UCDMidtermTest.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ucdmidtermtest;
+
+/**
+ *
+ * @author jrc62
+ */
+public class UCDMidtermTest {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("This is a test for the test");
+    }
+    
+}
